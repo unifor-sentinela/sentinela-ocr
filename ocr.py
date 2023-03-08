a3c48facd22ff8bc9184e1672849fe9a62d48789
PedroGaya <pedrohenriquegaya@gmail.com>
2023-03-08 18:12:34 -0300

    add mocked image processing

--- ocr.py
+++ ocr.py
@@ -1,11 +1,39 @@
 import json
+import time
 
 def process(bucket, key):
     try:
-        placeholder = {
-            bucket: key,
-        } 
-        result = json.dumps(placeholder)
-        return result
+        data = {
+            "filename": key,
+            "data": {
+                "raw": "",
+                "saturation": {
+                    "value": 0,
+                    "alarm": False
+                },
+                "heartRate": {
+                    "value": 0,
+                    "alarm": False
+                },
+                "respiratoryRate": {
+                    "value": 0,
+                    "alarm": False
+                },
+                "temperature": {
+                    "value": 0,
+                    "alarm": False
+                },
+                "systolicPressure": {
+                    "value": 0,
+                    "alarm": False
+                },
+                "diastolicPressure": {
+                    "value": 0,
+                    "alarm": False
+                }
+            }
+        }
+        time.sleep(3)
+        return json.dumps(data)
     except:
         raise
