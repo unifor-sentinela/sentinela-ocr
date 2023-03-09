948422f593dee55330ef6e33c2aad56b6dc317c7
PedroGaya <pedrohenriquegaya@gmail.com>
2023-03-09 19:21:05 -0300

    and env vars support

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+import os
 import boto3
 import json
 from ocr import process
 
-bucket_queue_name = "sentinela-sqs-bucket-ocr-queue-development"
-crud_queue_name = "sentinela-sqs-ocr-crud-queue-development"
+bucket_queue_name = os.getenv('BUCKET_QUEUE')
+crud_queue_name = os.getenv('CRUD_QUEUE')
 
 sqs = boto3.resource(
     service_name='sqs',
-    endpoint_url='http://localhost:4566',
-    aws_access_key_id='root',
-    aws_secret_access_key='root'
+    endpoint_url=os.getenv('AWS_ENDPOINT'),
+    aws_access_key_id=os.getenv('AWS_SECRET_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
     )
 
 bucket_queue = sqs.get_queue_by_name(QueueName=bucket_queue_name)
@@ -32,7 +33,11 @@ def process_message(message_body):
 
 if __name__ == "__main__":
     while True:
-        messages = bucket_queue.receive_messages(MaxNumberOfMessages=1)
+        print('Polling for messages...')
+        messages = bucket_queue.receive_messages(
+            MaxNumberOfMessages=1,
+            WaitTimeSeconds=20
+            )
         for message in messages:
             process_message(json.loads(message.body))
             message.delete()
